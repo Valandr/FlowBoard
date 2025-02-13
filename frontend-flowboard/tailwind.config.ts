4ca8231c29c7b0543f28dae7d3824747bb2eaa17
Valandr <valerie.andriani@gmail.com>
2025-02-13 17:59:44 +0100

    chore(config): setup & configure tailwind

---
+++ frontend-flowboard/tailwind.config.ts
@@ -0,0 +1,9 @@
+import type { Config } from 'tailwindcss'
+
+export default <Config>{
+  content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
+  theme: {
+    extend: {}
+  },
+  plugins: []
+}
