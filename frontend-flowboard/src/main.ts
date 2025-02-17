03d9703e074e7c43d6ef5ea191dd11aaa688d144
Valandr <valerie.andriani@gmail.com>
2025-02-17 19:42:23 +0100

    build(routes): setting up frontend routes

--- frontend-flowboard/src/main.ts
+++ frontend-flowboard/src/main.ts
@@ -1,7 +1,13 @@
-import './assets/main.css'
-
 import { createApp } from 'vue'
 import App from './App.vue'
 import './index.css'
+import { createPinia } from 'pinia'
+import router from './router/index'
+import './assets/main.css'
 
-createApp(App).mount('#app')
+const app = createApp(App)
+
+app.use(createPinia())
+app.use(router)
+
+app.mount('#app')
